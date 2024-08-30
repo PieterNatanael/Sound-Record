7411b2dac311d8ac0033736b6aa340bd31981150
PieterNatanael <pieternatanael@icloud.com>
2024-08-30 18:11:45 +0800

    Initial Commit

---
+++ Sound Record/Sound_RecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_RecordApp.swift
+//  Sound Record
+//
+//  Created by Pieter Yoshua Natanael on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Sound_RecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
